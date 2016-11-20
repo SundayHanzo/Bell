47c837871adee2c1b8e8a29dbae3aab71b5a1ec0
이지윤 <shy817@naver.com>
2016-11-20 16:01:05 +0900

    DestroyArrow

--- Assets/Script/Arrow.cs
+++ Assets/Script/Arrow.cs
@@ -16,16 +16,13 @@ public class Arrow : MonoBehaviour {
     public Vector3 downPosition;
     public Vector3 upPosition;
 
+    //timer
+    float TimeLeft = 3.0f;
+
 	// Use this for initialization
 	void Start () {
         //create arrow object
-        //position
-        arrowPosition.x = -0.2f;
-        arrowPosition.y = -3.7f;
-        arrowPosition.z = 11.7f;
-        //create
-        arrow = (GameObject)Instantiate(arrowPrefab, arrowPosition, Quaternion.identity);
-        arrow.transform.Rotate(-63.7f, -155f, -202f);
+        CreateArrow();
 
     }
 	
@@ -33,7 +30,29 @@ public class Arrow : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("Fire1")) GetTouchPos();
         if (Input.GetButtonUp("Fire1")) GetUpPos();
+
+        //timer count down
+        //TimeLeft -= Time.deltaTime;
+       
+        //if (TimeLeft == 0) {
+    
+        //    ArrowDelete();
+        //}
 	}
+
+
+    void CreateArrow() {
+        //set timer
+        TimeLeft = 3.0f;
+
+        //position
+        arrowPosition.x = -0.2f;
+        arrowPosition.y = -3.7f;
+        arrowPosition.z = 11.7f;
+        //create
+        arrow = (GameObject)Instantiate(arrowPrefab, arrowPosition, Quaternion.identity);
+        arrow.transform.Rotate(-63.7f, -155f, -202f);
+    }
 
     void GetTouchPos() {
         //get down position
@@ -52,10 +71,21 @@ public class Arrow : MonoBehaviour {
     }
 
     void Shot() {
+        //power
+        float power = downPosition.y - upPosition.y;
         //get Rigidbody
         Rigidbody arrowRigidBody = arrow.GetComponent<Rigidbody>();
         //add force
-        arrowRigidBody.AddForce(transform.forward * shotSpeed);
+        arrowRigidBody.AddForce(transform.forward * shotSpeed * power);
         arrowRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+        //delete current arrow and create new arrow after 3second
+        Invoke("ArrowDelete", 3);
+    }
+
+    void ArrowDelete() {
+        //destroy current arrow
+        Destroy(arrow);
+        //create new arrow
+        CreateArrow();
     }
 }
